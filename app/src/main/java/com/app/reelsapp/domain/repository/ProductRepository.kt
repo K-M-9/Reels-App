650cd130214b45e3962880ecbfdaf13778383971
Kareem Mohamed <63722@fa-hiss.edu.eg>
2025-07-23 16:42:07 +0300

    update fun name to get product owner.

--- app/src/main/java/com/app/reelsapp/domain/repository/ProductRepository.kt
+++ app/src/main/java/com/app/reelsapp/domain/repository/ProductRepository.kt
@@ -2,10 +2,10 @@ package com.app.reelsapp.domain.repository
 
 import androidx.paging.PagingData
 import com.app.reelsapp.domain.model.Product
-import com.app.reelsapp.domain.model.UserContent
+import com.app.reelsapp.domain.model.ProductOwner
 import kotlinx.coroutines.flow.Flow
 
 interface ProductRepository {
     fun getProducts(): Flow<PagingData<Product>>
-    fun getUserContent(): Flow<PagingData<UserContent>>
+    fun getProductOwner(): Flow<PagingData<ProductOwner>>
 }
