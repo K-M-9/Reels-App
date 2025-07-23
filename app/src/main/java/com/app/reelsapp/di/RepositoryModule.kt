0a57bbb3e35e95fd089aaf3ae991111842c5de89
Kareem Mohamed <63722@fa-hiss.edu.eg>
2025-07-23 19:01:13 +0300

    add di for reels feature.

--- app/src/main/java/com/app/reelsapp/di/RepositoryModule.kt
+++ app/src/main/java/com/app/reelsapp/di/RepositoryModule.kt
@@ -1,25 +1,42 @@
 package com.app.reelsapp.di
 
+import android.content.Context
 import com.app.reelsapp.data.LocalDataSource
 import com.app.reelsapp.data.local.LocalDataSourceImpl
+import com.app.reelsapp.data.local.database.dao.ProductDao
+import com.app.reelsapp.data.local.database.dao.UserDao
 import com.app.reelsapp.data.repository.ProductRepositoryImpl
+import com.app.reelsapp.data.repository.UserRepositoryImpl
 import com.app.reelsapp.domain.repository.ProductRepository
-import dagger.Binds
+import com.app.reelsapp.domain.repository.UserRepository
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-abstract class RepositoryModule {
+object RepositoryModule {
 
-    @Binds
-    abstract fun bindLocalDataSource(
-        localDataSourceImpl: LocalDataSourceImpl
-    ): LocalDataSource
+    @Provides
+    @Singleton
+    fun providesLocalDataSource(
+        @ApplicationContext context: Context,
+        productDao: ProductDao,
+        userDao: UserDao
+    ): LocalDataSource = LocalDataSourceImpl(context, productDao, userDao)
 
-    @Binds
-    abstract fun bindProductRepository(
-        productRepositoryImpl: ProductRepositoryImpl
-    ): ProductRepository
+    @Provides
+    @Singleton
+    fun providesProductRepository(
+        localDataSource: LocalDataSource
+    ): ProductRepository = ProductRepositoryImpl(localDataSource)
+
+    @Provides
+    @Singleton
+    fun providesUserRepository(
+        localDataSource: LocalDataSource
+    ): UserRepository = UserRepositoryImpl(localDataSource)
 }
