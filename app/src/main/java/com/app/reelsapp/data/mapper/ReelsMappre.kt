bec3a89d86d59def237282fb90514a9eacbe684c
Kareem Mohamed <63722@fa-hiss.edu.eg>
2025-07-23 18:50:56 +0300

    update mapper for favorite and follow.

--- app/src/main/java/com/app/reelsapp/data/mapper/ReelsMappre.kt
+++ app/src/main/java/com/app/reelsapp/data/mapper/ReelsMappre.kt
@@ -1,26 +1,35 @@
 package com.app.reelsapp.data.mapper
 
-import com.app.reelsapp.data.local.dto.ProductDto
-import com.app.reelsapp.data.local.dto.UserContentDto
+import com.app.reelsapp.data.local.database.dto.ProductDto
+import com.app.reelsapp.data.local.database.dto.ProductOwnerDto
+import com.app.reelsapp.data.local.database.dto.UserDto
 import com.app.reelsapp.domain.model.Product
-import com.app.reelsapp.domain.model.UserContent
+import com.app.reelsapp.domain.model.ProductOwner
+import com.app.reelsapp.domain.model.User
 
-fun ProductDto.toDomain(): Product = Product(
+fun ProductDto.toDomain(isFavorite: Boolean): Product = Product(
     id = id,
     name = name,
     description = description,
     price = price,
+    isFavorite = isFavorite,
     videoUrl = video_url
 )
 
-fun UserContentDto.toDomain(): UserContent = UserContent(
+fun ProductOwnerDto.toDomain(isFollow: Boolean): ProductOwner = ProductOwner(
     id = id,
     userId = user_id,
     username = name,
     title = title,
     content = content,
+    isFollow = isFollow,
     rating = rating,
     createdAt = created_at,
     productId = product_id,
     imageUrl = image_url
+)
+
+fun UserDto.toDomain(): User = User(
+    name = name,
+    username = username,
 )
